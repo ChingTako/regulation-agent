9f251a08b8c11e32e8e883b2dbb5df13358d8aa9
Ching Chang <ms112241712@gmail.com>
2026-06-05 16:19:12 +0800

    github actions setup

--- notifier/telegram.py
+++ notifier/telegram.py
@@ -1,13 +1,17 @@
+import os
 import requests
 
-BOT_TOKEN = "你的token"
-CHAT_ID = "8546764711"
+BOT_TOKEN = os.environ["8958313466:AAE9tVCNlISwY6-3GsnyMFEiDvP35SENU4E"]
+CHAT_ID = os.environ["8546764711"]
 
 def send_telegram(text):
 
     url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
 
-    requests.post(url, data={
-        "chat_id": CHAT_ID,
-        "text": text
-    })
+    requests.post(
+        url,
+        data={
+            "chat_id": CHAT_ID,
+            "text": text
+        }
+    )
